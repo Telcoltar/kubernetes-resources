d230198d57a3920e78370d3dfbe1a9462caf5401
Telcoltar <54867569+Telcoltar@users.noreply.github.com>
2025-11-11 12:00:41 +0100

    chore: fix typo

--- builders/configmap.go
+++ builders/configmap.go
@@ -7,30 +7,30 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-type ConfigmapBuilder struct {
-	*objectMetaBuilder[ConfigmapBuilder]
+type ConfigMapBuilder struct {
+	*objectMetaBuilder[ConfigMapBuilder]
 	data map[string]string
 }
 
-func ConfigMap(name string) *ConfigmapBuilder {
-	b := &ConfigmapBuilder{
+func ConfigMap(name string) *ConfigMapBuilder {
+	b := &ConfigMapBuilder{
 		data: map[string]string{},
 	}
 	b.objectMetaBuilder = ObjectMeta(b).name(name)
 	return b
 }
 
-func (b *ConfigmapBuilder) Data(data map[string]string) *ConfigmapBuilder {
+func (b *ConfigMapBuilder) Data(data map[string]string) *ConfigMapBuilder {
 	maps.Copy(b.data, data)
 	return b
 }
 
-func (b *ConfigmapBuilder) Datum(key, value string) *ConfigmapBuilder {
+func (b *ConfigMapBuilder) Datum(key, value string) *ConfigMapBuilder {
 	b.data[key] = value
 	return b
 }
 
-func (b *ConfigmapBuilder) Build() (runtime.Object, error) {
+func (b *ConfigMapBuilder) Build() (runtime.Object, error) {
 	return &corev1.ConfigMap{
 		TypeMeta:   configMapType,
 		ObjectMeta: b.objectMetaBuilder.Build(),
@@ -38,6 +38,6 @@ func (b *ConfigmapBuilder) Build() (runtime.Object, error) {
 	}, nil
 }
 
-func (b *ConfigmapBuilder) Env(name string) *EnvKeyValuesBuilder {
+func (b *ConfigMapBuilder) Env(name string) *EnvKeyValuesBuilder {
 	return Env(name).FromConfigmap(b.meta.Name)
 }
